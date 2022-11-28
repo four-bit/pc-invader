c8f98715f9f8848db286688d9457d5d497dcc366
miketvo <ikxplicitie@gmail.com>
2022-11-28 11:24:07 +0700

    Added test player sprite

--- core/src/com/fourbit/pc_invader/PcInvader.java
+++ core/src/com/fourbit/pc_invader/PcInvader.java
@@ -11,46 +11,51 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class PcInvader extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture background;
+    private Texture player;
     private OrthographicCamera viewportCamera;
     private FrameBuffer sceneFrameBuffer;
 
-    public static final int FRAMEBUFFER_WIDTH = 1920;
-    public static final int FRAMEBUFFER_HEIGHT = 1080;
-    private static final int UPSCALE = 6;
-    public static final int SCREEN_WIDTH = FRAMEBUFFER_WIDTH * UPSCALE;
-    public static final int SCREEN_HEIGHT = FRAMEBUFFER_HEIGHT * UPSCALE;
+    public static final int GAME_WIDTH = 1920;
+    public static final int GAME_HEIGHT = 1080;
+
+    private int i;
 
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         background = new Texture("background.png");
+        player = new Texture("player/lvl3-default.png");
 
-        sceneFrameBuffer = new FrameBuffer(Pixmap.Format.RGB888, FRAMEBUFFER_WIDTH, FRAMEBUFFER_HEIGHT, false);
+        sceneFrameBuffer = new FrameBuffer(Pixmap.Format.RGB888, GAME_WIDTH, GAME_HEIGHT, false);
         sceneFrameBuffer.getColorBufferTexture().setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
 
-        viewportCamera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        viewportCamera = new OrthographicCamera(GAME_WIDTH, GAME_HEIGHT);
         viewportCamera.position.set(0.5f * viewportCamera.viewportWidth, 0.5f * viewportCamera.viewportHeight, 0.0f);
         viewportCamera.update();
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.0f, 0.0f, 0.0f, 1.0f);
+        ScreenUtils.clear(0.0f, 0.0f, 0.0f, 0.0f);
 
         Gdx.gl20.glEnable(GL20.GL_SCISSOR_TEST); // re-enabled each frame because UI changes GL state
         Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        batch.setProjectionMatrix(viewportCamera.combined);
+
         sceneFrameBuffer.begin();
         batch.begin();
         batch.draw(background, 0, 0);
+        batch.draw(player, i % GAME_WIDTH, i % GAME_HEIGHT);
+        i++;
         batch.end();
         sceneFrameBuffer.end();
 
         batch.begin();
         batch.setColor(1.0f, 1.0f, 1.0f, 1.0f);
-        batch.draw(sceneFrameBuffer.getColorBufferTexture(), 0, SCREEN_HEIGHT, SCREEN_WIDTH, -SCREEN_HEIGHT);
+        batch.draw(sceneFrameBuffer.getColorBufferTexture(), 0, GAME_HEIGHT, GAME_WIDTH, -GAME_HEIGHT);
         batch.end();
     }
 
@@ -59,5 +64,6 @@ public class PcInvader extends ApplicationAdapter {
         batch.dispose();
         sceneFrameBuffer.dispose();
         background.dispose();
+        player.dispose();
     }
 }
--- desktop/src/com/fourbit/pc_invader/DesktopLauncher.java
+++ desktop/src/com/fourbit/pc_invader/DesktopLauncher.java
@@ -3,7 +3,7 @@ package com.fourbit.pc_invader;
 import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.fourbit.pc_invader.PcInvader;
+
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
