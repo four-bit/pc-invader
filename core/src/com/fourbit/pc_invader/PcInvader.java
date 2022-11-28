56da7b6262937645e484ec7b9252245417995299
miketvo <ikxplicitie@gmail.com>
2022-11-28 08:53:19 +0700

    Got PcInvader background.png to work in 1920x1080 resolution

--- core/src/com/fourbit/pc_invader/PcInvader.java
+++ core/src/com/fourbit/pc_invader/PcInvader.java
@@ -1,31 +1,63 @@
 package com.fourbit.pc_invader;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+
 public class PcInvader extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture img;
+    private SpriteBatch batch;
+    private Texture background;
+    private OrthographicCamera viewportCamera;
+    private FrameBuffer sceneFrameBuffer;
+
+    public static final int FRAMEBUFFER_WIDTH = 1920;
+    public static final int FRAMEBUFFER_HEIGHT = 1080;
+    private static final int UPSCALE = 6;
+    public static final int SCREEN_WIDTH = FRAMEBUFFER_WIDTH * UPSCALE;
+    public static final int SCREEN_HEIGHT = FRAMEBUFFER_HEIGHT * UPSCALE;
+
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+        background = new Texture("background.png");
+
+        sceneFrameBuffer = new FrameBuffer(Pixmap.Format.RGB888, FRAMEBUFFER_WIDTH, FRAMEBUFFER_HEIGHT, false);
+        sceneFrameBuffer.getColorBufferTexture().setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
+
+        viewportCamera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        viewportCamera.position.set(0.5f * viewportCamera.viewportWidth, 0.5f * viewportCamera.viewportHeight, 0.0f);
+        viewportCamera.update();
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(1, 0, 0, 1);
+        ScreenUtils.clear(0.0f, 0.0f, 0.0f, 1.0f);
+
+        Gdx.gl20.glEnable(GL20.GL_SCISSOR_TEST); // re-enabled each frame because UI changes GL state
+        Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        sceneFrameBuffer.begin();
         batch.begin();
-        batch.draw(img, 0, 0);
+        batch.draw(background, 0, 0);
+        batch.end();
+        sceneFrameBuffer.end();
+
+        batch.begin();
+        batch.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+        batch.draw(sceneFrameBuffer.getColorBufferTexture(), 0, SCREEN_HEIGHT, SCREEN_WIDTH, -SCREEN_HEIGHT);
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        img.dispose();
+        sceneFrameBuffer.dispose();
+        background.dispose();
     }
 }
--- desktop/src/com/fourbit/pc_invader/DesktopLauncher.java
+++ desktop/src/com/fourbit/pc_invader/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.fourbit.pc_invader;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.fourbit.pc_invader.PcInvader;
@@ -7,9 +8,12 @@ import com.fourbit.pc_invader.PcInvader;
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
     public static void main(String[] arg) {
+        Graphics.DisplayMode primaryMode = Lwjgl3ApplicationConfiguration.getDisplayMode();
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setForegroundFPS(60);
-        config.setTitle("pc-invader");
+        config.setFullscreenMode(primaryMode);
+        config.useVsync(true);
+        config.setTitle("PC INVADER");
         new Lwjgl3Application(new PcInvader(), config);
     }
 }
