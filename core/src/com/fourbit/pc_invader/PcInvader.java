4b5a3449f250d02099cb8dec2f88698e4a5965f9
miketvo <ikxplicitie@gmail.com>
2022-12-01 19:54:05 +0700

    Further implemented Player class and fixed typos on line 59 and 60 that caused boundary checking to malfunction

--- core/src/com/fourbit/pc_invader/PcInvader.java
+++ core/src/com/fourbit/pc_invader/PcInvader.java
@@ -16,7 +16,6 @@ public class PcInvader extends ApplicationAdapter {
     private FrameBuffer sceneFrameBuffer;
     private Player player;
 
-
     public static final int GAME_WIDTH = 1920;
     public static final int GAME_HEIGHT = 1080;
 
@@ -25,7 +24,15 @@ public class PcInvader extends ApplicationAdapter {
     public void create() {
         batch = new SpriteBatch();
         background = new Texture("background.png");
-        player = new Player(GAME_WIDTH / 2, GAME_HEIGHT / 2, 200, "player/lvl3-default.png");
+        player = new Player(
+                GAME_WIDTH / 2,
+                GAME_HEIGHT / 2,
+                500,
+                8,
+                3,
+                true,
+                "player/lvl3-default.png"
+        );
 
         sceneFrameBuffer = new FrameBuffer(Pixmap.Format.RGB888, GAME_WIDTH, GAME_HEIGHT, false);
         sceneFrameBuffer.getColorBufferTexture().setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
@@ -37,20 +44,20 @@ public class PcInvader extends ApplicationAdapter {
 
     @Override
     public void render() {
-        int calcPlayerSpeed = (int) (player.getSpeed() * Gdx.graphics.getDeltaTime());
+        int playerMovement = (int) (player.getSpeed() * Gdx.graphics.getDeltaTime());
         if (Gdx.input.isKeyPressed(Input.Keys.A))
-            player.setX(player.getX() - calcPlayerSpeed);
+            player.setX(player.getX() - playerMovement);
         if (Gdx.input.isKeyPressed(Input.Keys.D))
-            player.setX(player.getX() + calcPlayerSpeed);
+            player.setX(player.getX() + playerMovement);
         if (Gdx.input.isKeyPressed(Input.Keys.W))
-            player.setY(player.getY() + calcPlayerSpeed);
+            player.setY(player.getY() + playerMovement);
         if (Gdx.input.isKeyPressed(Input.Keys.S))
-            player.setY(player.getY() - calcPlayerSpeed);
+            player.setY(player.getY() - playerMovement);
 
-        if (player.getX() - player.getWidth() / 2 < 0) player.setX(player.getWidth() / 2);
-        if (player.getX() + player.getWidth() / 2 > GAME_WIDTH) player.setX(GAME_WIDTH - player.getWidth() / 2);
-        if (player.getX() - player.getHeight() / 2 < 0) player.setY(player.getHeight() / 2);
-        if (player.getX() + player.getHeight() / 2 > GAME_HEIGHT) player.setY(GAME_HEIGHT - player.getHeight() / 2);
+        if ((player.getX() - player.getWidth() / 2) < 0) player.setX(player.getWidth() / 2);
+        if ((player.getX() + player.getWidth() / 2) > GAME_WIDTH) player.setX(GAME_WIDTH - player.getWidth() / 2);
+        if ((player.getY() - player.getHeight() / 2) < 0) player.setY(player.getHeight() / 2);
+        if ((player.getY() + player.getHeight() / 2) > GAME_HEIGHT) player.setY(GAME_HEIGHT - player.getHeight() / 2);
 
         ScreenUtils.clear(0.0f, 0.0f, 0.0f, 0.0f);
 
--- core/src/com/fourbit/pc_invader/Player.java
+++ core/src/com/fourbit/pc_invader/Player.java
@@ -1,33 +1,106 @@
 package com.fourbit.pc_invader;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.sun.tools.javac.main.Option;
+
 
 public class Player {
     private Texture texture;
-    private int x, y, speed;
+    private int x, y, speed, healthPoints, shieldPoints;
+    private boolean hasShield;
 
     Player() {
-        texture = null;
+        this.texture = null;
         this.x = 0;
         this.y = 0;
-        speed = 0;
+        this.speed = 0;
+        this.healthPoints = 0;
+        this.shieldPoints = -1;
+        this.hasShield = false;
     }
 
-    Player(int x, int y, int speed, String spriteInternalPath) {
+    Player(
+            int x, int y,
+            int speed,
+            int maxHealth,
+            int maxShield, boolean hasShield,
+            String spriteInternalPath) {
         this.x = x;
         this.y = y;
         this.speed = speed;
+        this.healthPoints = maxHealth;
+        this.shieldPoints = maxShield;
+        this.hasShield = hasShield;
         this.texture = new Texture(spriteInternalPath);
     }
 
-    public int getX() { return x; }
-    public void setX(int x) { this.x = x; }
-    public int getY() { return y; }
-    public void setY(int y) { this.y = y; }
-    public int getSpeed() { return this.speed; }
-    public int getWidth() { return this.texture.getWidth(); }
-    public int getHeight() { return this.texture.getHeight(); }
-    public Texture getTexture() { return this.texture; }
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getSpeed() {
+        return this.speed;
+    }
+
+    public int getHealthPoints() {
+        return this.healthPoints;
+    }
+
+    public int getShieldPoints() {
+        return this.hasShield ? this.shieldPoints : -1;
+    }
+
+    public Texture getTexture() {
+        return this.texture;
+    }
+
+    public int getWidth() {
+        return this.texture.getWidth();
+    }
+
+    public int getHeight() {
+        return this.texture.getHeight();
+    }
+
+    public void setX(int val) {
+        this.x = val;
+    }
+
+    public void setY(int val) {
+        this.y = val;
+    }
+
+    public void setSpeed(int val) {
+        this.speed = val;
+    }
+
+    public void setHealthPoints(int val) throws Option.InvalidValueException {
+        if (val >= 0) {
+            this.healthPoints = val;
+        } else {
+            throw new Option.InvalidValueException("val cannot be negative.");
+        }
+    }
+
+    public void setShieldPoints(int val) throws Option.InvalidValueException {
+        if (val >= 0) {
+            if (!this.hasShield) {
+                this.hasShield = true;
+            }
+            this.shieldPoints = val;
+        } else {
+            throw new Option.InvalidValueException("val cannot be negative.");
+        }
+    }
+
+    public void disableShield() {
+        this.hasShield = false;
+        this.shieldPoints = -1;
+    }
 
 
     public void dispose() {
