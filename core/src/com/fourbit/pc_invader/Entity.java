874f4a2d55d5d1b954079b55c68e8aaa2289aeaf
miketvo <ikxplicitie@gmail.com>
2022-12-16 14:36:58 +0700

    Code formatting

--- core/src/com/fourbit/pc_invader/Entity.java
+++ core/src/com/fourbit/pc_invader/Entity.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Vector2;
 
+
 public class Entity {
     private final Vector2 position;
     private float angle;
