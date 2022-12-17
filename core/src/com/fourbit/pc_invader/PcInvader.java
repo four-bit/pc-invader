9a98f527ebbfd6d5d103c1684851cd46854a27e0
miketvo <ikxplicitie@gmail.com>
2022-12-17 12:15:29 +0700

    Optimized imports in PcInvader

--- core/src/com/fourbit/pc_invader/PcInvader.java
+++ core/src/com/fourbit/pc_invader/PcInvader.java
@@ -2,7 +2,10 @@ package com.fourbit.pc_invader;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.math.Vector2;
